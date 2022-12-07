f6267ad9ba9d31b0329acc8e86f8218cd983755a
Alysia Huggins <hi@alysia.tech>
2022-12-07 06:51:24 -0800

    this folder is no longer needed - use quiz-app-ui instead

--- 04-zkapp-browser-ui-temp/contracts/curriculum/curriculum.js
+++
@@ -1,8 +0,0 @@
-import  {default as questions}  from "./questions.json" assert { type: "json" };
-import  {default as questionsRadio}  from "./questionsRadio.json" assert { type: "json" };
-
-import  {default as answers}  from "./answers.json" assert { type: "json" };
-
-export { questions, answers,  questionsRadio};
-
-
